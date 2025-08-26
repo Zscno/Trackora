08bc16a7d3d027786d9cfeda82e630838b6ea3b3
Zscno <zscNofQSpxJHp@outlook.com>
2025-08-26 18:15:28 +0800

    🔧修复：为类型 ProcessInfo 的所有属性设定默认值

--- Trackora/WindowTracker.cs
+++ Trackora/WindowTracker.cs
@@ -31,23 +31,23 @@ internal class ProcessInfo
 	/// <summary>
 	/// 显示给用户的名称。
 	/// </summary>
-	public string DisplayName { get; set; }
+	public string DisplayName { get; set; } = string.Empty;
 
 	/// <summary>
 	/// 图标的Uri。
 	/// </summary>
-	public string IconUri { get; set; }
+	public string IconUri { get; set; } = string.Empty;
 
 	/// <summary>
 	/// 进程名称。
 	/// </summary>
-	public string ProcessName { get; set; }
+	public string ProcessName { get; set; } = string.Empty;
 
 	/// <summary>
 	/// 使用时长。
 	/// </summary>
 	[JsonIgnore]
-	public string UsedTime { get; set; }
+	public string UsedTime { get; set; } = string.Empty;
 }
 
 internal class WindowTracker
