82c5f60d16823a5c73040785c06288b3ff93a693
Zscno <zscNofQSpxJHp@outlook.com>
2026-08-12 21:50:15 +0800

    ♻重构：为 ProcessInfo 类型构造函数添加注释

--- Trackora/ProcessInfo.cs
+++ Trackora/ProcessInfo.cs
@@ -13,6 +13,10 @@ namespace Zscno.Trackora
         [JsonInclude]
         internal string DisplayName { get; set; }
 
+        /// <summary>
+        /// 初始化一个新的 <see cref="ProcessInfo"/> 实例。
+        /// </summary>
+        /// <param name="displayName">显示给用户的名称。</param>
         internal ProcessInfo(string displayName)
         {
             DisplayName = displayName;
