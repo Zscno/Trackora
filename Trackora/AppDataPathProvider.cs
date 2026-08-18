6460afe4d0d895c0290ba309854fdefa5b04ce77
Zscno <zscNofQSpxJHp@outlook.com>
2026-08-18 21:56:54 +0800

    ♻重构：更正 AppDataPathProvider 类型的注释

--- Trackora/AppDataPathProvider.cs
+++ Trackora/AppDataPathProvider.cs
@@ -6,16 +6,16 @@ namespace Zscno.Trackora
     ///<inheritdoc cref="IAppDataPathProvider"/>
     internal class AppDataPathProvider : IAppDataPathProvider
     {
-        ///<inheritdoc cref="IAppDataPathProvider"/>
+        ///<inheritdoc cref="IAppDataPathProvider.IconPath"/>
         public string IconPath { get; }
 
-        ///<inheritdoc cref="IAppDataPathProvider"/>
+        ///<inheritdoc cref="IAppDataPathProvider.LocalCachePath"/>
         public string LocalCachePath { get; }
 
-        ///<inheritdoc cref="IAppDataPathProvider"/>
+        ///<inheritdoc cref="IAppDataPathProvider.LocalPath"/>
         public string LocalPath { get; }
 
-        ///<inheritdoc cref="IAppDataPathProvider"/>
+        ///<inheritdoc cref="IAppDataPathProvider.RecordPath"/>
         public string RecordPath { get; }
 
         internal AppDataPathProvider()
