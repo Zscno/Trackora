c5b7b770497a803b791917d62ed553d638897420
Zscno <zscNofQSpxJHp@outlook.com>
2026-08-18 20:28:06 +0800

    🧩重构：添加 AppDataPathProvider 类型

---
+++ Trackora/AppDataPathProvider.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Windows.Storage;
+using System.IO;
+
+namespace Zscno.Trackora
+{
+    ///<inheritdoc cref="IAppDataPathProvider"/>
+    internal class AppDataPathProvider : IAppDataPathProvider
+    {
+        ///<inheritdoc cref="IAppDataPathProvider"/>
+        public string IconPath { get; }
+
+        ///<inheritdoc cref="IAppDataPathProvider"/>
+        public string LocalCachePath { get; }
+
+        ///<inheritdoc cref="IAppDataPathProvider"/>
+        public string LocalPath { get; }
+
+        ///<inheritdoc cref="IAppDataPathProvider"/>
+        public string RecordPath { get; }
+
+        internal AppDataPathProvider()
+        {
+            var appData = ApplicationData.GetDefault();
+            LocalCachePath = appData.LocalCachePath;
+            LocalPath = appData.LocalPath;
+            IconPath = Path.Combine(LocalCachePath, "Icons");
+            RecordPath = Path.Combine(LocalPath, "Records");
+        }
+    }
+}
---
+++ Trackora/IAppDataPathProvider.cs
@@ -0,0 +1,28 @@
+﻿namespace Zscno.Trackora
+{
+    /// <summary>
+    /// 用于提供应用程序的数据存储路径。
+    /// </summary>
+    internal interface IAppDataPathProvider
+    {
+        /// <summary>
+        /// 用于存放获取到的应用程序图标的文件夹路径。
+        /// </summary>
+        string IconPath { get; }
+
+        /// <summary>
+        /// 不可备份的本地数据文件夹路径。
+        /// </summary>
+        string LocalCachePath { get; }
+
+        /// <summary>
+        /// 可备份的本地数据文件夹路径。
+        /// </summary>
+        string LocalPath { get; }
+
+        /// <summary>
+        /// 用于存放用户使用记录的文件夹路径。
+        /// </summary>
+        string RecordPath { get; }
+    }
+}
