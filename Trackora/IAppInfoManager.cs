385f0757b5cc8a5c498c19b37ff32627fec54f2b
Zscno <zscNofQSpxJHp@outlook.com>
2026-08-19 20:47:06 +0800

    🧩重构：将 AppInfoManager 改为实例类型

---
+++ Trackora/IAppInfoManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Concurrent;
+using System.Diagnostics;
+
+namespace Zscno.Trackora
+{
+    /// <summary>
+    /// 提供获取和保存应用程序信息的功能。
+    /// </summary>
+    internal interface IAppInfoManager : IDataLoadable, IDataStorable
+    {
+        /// <summary>
+        /// 应用程序的信息映射表，键为进程的名称。
+        /// </summary>
+        ConcurrentDictionary<string, ProcessInfo> AppInfoMap { get; } // TODO: 使用 AppInfo。
+
+        /// <summary>
+        /// 获取指定应用程序的图标 URI。
+        /// </summary>
+        /// <remarks>若在图标文件夹未找到指定应用程序的图标，将使用系统的默认应用程序图标。</remarks>
+        /// <param name="processName">进程的名称。</param>
+        /// <returns>获取到的图标 URI。</returns>
+        string GetAppIconUri(string processName);
+
+        /// <summary>
+        /// 缓存指定应用程序的信息，包括应用程序的显示名称和图标。
+        /// </summary>
+        /// <param name="windowHandle">进程关联的窗口句柄。</param>
+        /// <param name="process">     进程的 <see cref="Process"/> 组件。</param>
+        void CacheAppInfo(nint windowHandle, Process process);
+    }
+}
