fdf6340de1cabd63c8ce9bc616c3c2cbc0c21b9c
Zscno <zscNofQSpxJHp@outlook.com>
2026-08-21 11:35:46 +0800

    🧩重构：将延迟保存使用记录迁移到 UsageRecordManager 类型

--- Trackora/IUsageRecordManager.cs
+++ Trackora/IUsageRecordManager.cs
@@ -1,13 +1,20 @@
-﻿namespace Zscno.Trackora
+﻿using System;
+
+namespace Zscno.Trackora
 {
     /// <summary>
     /// 提供获取和保存使用记录的功能。
     /// </summary>
-    internal interface IUsageRecordManager : IDataLoadable, IDataStorable
+    internal interface IUsageRecordManager : IDataLoadable, IDataStorable, IDisposable
     {
         /// <summary>
         /// 获取今天用户的使用记录。
         /// </summary>
         UsageRecord Record { get; }
+
+        /// <summary>
+        /// 请求保存当前的使用记录。
+        /// </summary>
+        void RequestSave();
     }
 }
--- Trackora/UsageRecordManager.cs
+++ Trackora/UsageRecordManager.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Zscno.Trackora
 {
     /// <inheritdoc cref="IUsageRecordManager"/>
-    internal class UsageRecordManager : IUsageRecordManager
+    internal partial class UsageRecordManager : IUsageRecordManager
     {
-        private string _recordFilePath;
-
         private readonly string _recordFolderPath;
+
+        /// <summary>
+        /// 用于保存使用记录的计时器。
+        /// </summary>
+        private readonly Timer _saveTimer;
+
+        private string _recordFilePath;
 
         public UsageRecord Record { get; private set; }
 
@@ -18,6 +24,15 @@ namespace Zscno.Trackora
             _recordFolderPath = pathProvider.RecordPath;
             _recordFilePath = Path.Combine(_recordFolderPath, $"{DateTime.Now: yyyy-MM-dd}.json");
             Record = new UsageRecord();
+            _saveTimer = new Timer(SaveLatest, null, Timeout.Infinite, Timeout.Infinite);
+        }
+
+        /// <summary>
+        /// 释放当前 <see cref="UsageRecordManager"/> 实例使用的所有资源。
+        /// </summary>
+        public void Dispose()
+        {
+            _saveTimer.Dispose();
         }
 
         /// <summary>
@@ -31,7 +46,7 @@ namespace Zscno.Trackora
             }
 
             string recordFilePath = Path.Combine(_recordFolderPath, $"{DateTime.Now: yyyy-MM-dd}.json");
-            if(recordFilePath!= _recordFilePath)
+            if (recordFilePath != _recordFilePath)
             {
                 Record = new UsageRecord();
                 _recordFilePath = recordFilePath;
@@ -47,6 +62,13 @@ namespace Zscno.Trackora
             return Task.CompletedTask;
         }
 
+        /// <inheritdoc cref="IUsageRecordManager.RequestSave"/>
+        /// <remarks>该方法应该在使用记录更新后调用，将会延迟 2 秒保存。</remarks>
+        public void RequestSave()
+        {
+            _ = _saveTimer.Change(2000, Timeout.Infinite);
+        }
+
         /// <summary>
         /// 保存用户今天的使用记录。
         /// </summary>
@@ -55,6 +77,19 @@ namespace Zscno.Trackora
             _ = Directory.CreateDirectory(_recordFolderPath);
             _ = Json.WriteJsonFile(_recordFilePath, Record, SourceGenerationContext.Default.UsageRecord);
             return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// 保存最新的使用记录。
+        /// </summary>
+        /// <remarks>该函数是 <see cref="_saveTimer"/> 的回调函数。</remarks>
+        private void SaveLatest(object? state)
+        {
+            _ = new SafeCaller()
+            {
+                LogMessage = "保存使用记录失败。",
+                RemindingMsgResKey = "ECanNotSetRecord",
+            }.CallMethodR(async () => await StoreAsync());
         }
     }
 }
