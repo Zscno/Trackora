c91e6cc7e73fb3958727f1c8ae1db4a0d422f209
Zscno <zscNofQSpxJHp@outlook.com>
2026-08-21 11:09:23 +0800

    🧩重构：将 ReminderManager 改为实例类型

---
+++ Trackora/IReminderManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Zscno.Trackora
+{
+    /// <summary>
+    /// 提供提醒用户的功能。
+    /// </summary>
+    internal interface IReminderManager : IDisposable
+    {
+        /// <summary>
+        /// 若总使用时间未达到阈值且从未发送过，则发送总使用时间提醒。
+        /// </summary>
+        void SendDailyIfNeeded();
+
+        /// <summary>
+        /// 停止所有提醒（包括总使用时间提醒和连续使用时间提醒）。
+        /// </summary>
+        void StopAll();
+
+        /// <summary>
+        /// 更新总使用时间提醒的到期时间。
+        /// </summary>
+        void UpdateDailyDueTime();
+
+        /// <summary>
+        /// 更新连续使用时间提醒的到期时间。
+        /// </summary>
+        void UpdateSessionDueTime();
+    }
+}
--- Trackora/ReminderManager.cs
+++ Trackora/ReminderManager.cs
@@ -1,14 +1,97 @@
 ﻿using Microsoft.Windows.AppNotifications;
 using Microsoft.Windows.AppNotifications.Builder;
 using System;
+using System.Threading;
 
 namespace Zscno.Trackora
 {
-    /// <summary>
-    /// 为提醒用户提供相关操作。
-    /// </summary>
-    internal static class ReminderManager
+    /// <inheritdoc cref="IReminderManager"/>
+    internal partial class ReminderManager : IReminderManager
     {
+        /// <summary>
+        /// 用于发送总使用时间提醒的计时器。
+        /// </summary>
+        private readonly Timer _dailyTimer;
+
+        /// <summary>
+        /// 用于发送连续使用时间提醒的计时器。
+        /// </summary>
+        private readonly Timer _sessionTimer;
+
+        /// <inheritdoc cref="ISettings"/>
+        private readonly ISettings _settings;
+
+        /// <inheritdoc cref="IUsageRecordManager"/>
+        private readonly IUsageRecordManager _usageRecordManager;
+
+        /// <summary>
+        /// 指示是否发送了总使用时间提醒。
+        /// </summary>
+        private bool _isDailySent;
+
+        public ReminderManager(ISettings settings, IUsageRecordManager usageRecordManager/*TODO: 接收日志实例。*/)
+        {
+            _isDailySent = false;
+            _settings = settings;
+            _usageRecordManager = usageRecordManager;
+            _dailyTimer = new Timer(SendDueDailyReminder, null, Timeout.Infinite, Timeout.Infinite);
+            _sessionTimer = new Timer(SendDueSessionReminder, null, Timeout.Infinite, Timeout.Infinite);
+        }
+
+        /// <summary>
+        /// 释放当前 <see cref="ReminderManager"/> 实例使用的所有资源。
+        /// </summary>
+        public void Dispose()
+        {
+            _dailyTimer.Dispose();
+            _sessionTimer.Dispose();
+        }
+
+        /// <inheritdoc cref="IReminderManager.SendDailyIfNeeded"/>
+        /// <remarks>该方法只应该在应用程序启动时调用。</remarks>
+        public void SendDailyIfNeeded()
+        {
+            if (_isDailySent)
+            {
+                return;
+            }
+
+            if (_usageRecordManager.Record.DailyDuration < _settings.DailyThreshold)
+            {
+                return;
+            }
+
+            _ = _dailyTimer.Change(0, Timeout.Infinite);
+            _isDailySent = true;
+        }
+
+        public void StopAll()
+        {
+            _ = _dailyTimer.Change(Timeout.Infinite, Timeout.Infinite);
+            _ = _sessionTimer.Change(Timeout.Infinite, Timeout.Infinite);
+        }
+
+        public void UpdateDailyDueTime()
+        {
+            if (_isDailySent)
+            {
+                return;
+            }
+
+            uint dueTime = _usageRecordManager.Record.DailyDuration < _settings.DailyThreshold ?
+                _settings.DailyThreshold - _usageRecordManager.Record.DailyDuration : 0;
+
+            _ = _dailyTimer.Change(dueTime, Timeout.Infinite);
+        }
+
+        public void UpdateSessionDueTime()
+        {
+            uint dueTime = _usageRecordManager.Record.SessionDuration < _settings.SessionThreshold ?
+                _settings.SessionThreshold - _usageRecordManager.Record.SessionDuration : 0;
+
+            _ = _sessionTimer.Change(dueTime, _settings.SessionThreshold);
+        }
+
         /// <summary>
         /// 发送指定的通知。
         /// </summary>
@@ -17,7 +100,7 @@ namespace Zscno.Trackora
         /// <param name="content">        通知的文本内容。</param>
         /// <param name="expirationTime"> 通知保留在通知中心的时间（以毫秒为单位）。</param>
         /// <param name="expiresOnReboot">指示通知是否会在重新启动后保留在通知中心。</param>
-        internal static void SendNotification(string title,
+        private static void SendNotification(string title,
                                               string content,
                                               uint expirationTime,
                                               bool expiresOnReboot)
@@ -40,6 +123,45 @@ namespace Zscno.Trackora
                 LogSystem.WriteLog(LogLevel.Warning,
                     $"通知发送失败。\n\tTitle={title},\n\tContent={content}\n\n{ex}");
             }
+        }
+
+        /// <summary>
+        /// 发送到期的总使用时间提醒。
+        /// </summary>
+        /// <remarks>该函数是 <see cref="_dailyTimer"/> 的回调函数。</remarks>
+        private void SendDueDailyReminder(object? state)
+        {
+            if (_isDailySent)
+            {
+                return;
+            }
+
+            SendNotification(
+                App.Loader.GetString("UsageTimeReminderTitle"),
+                App.Loader.GetString("TotalReminderText1") +
+                Localization.ToLocalizedTimeString(_usageRecordManager.Record.DailyDuration) +
+                App.Loader.GetString("TotalReminderText2"),
+                _settings.IdleThreshold,
+                false);
+
+            _isDailySent = true;
+        }
+
+        /// <summary>
+        /// 发送到期的连续使用时间提醒。
+        /// </summary>
+        /// <remarks>该函数是 <see cref="_sessionTimer"/> 的回调函数。</remarks>
+        private void SendDueSessionReminder(object? state)
+        {
+            SendNotification(
+                App.Loader.GetString("UsageTimeReminderTitle"),
+                App.Loader.GetString("ContinuousReminderText1") +
+                Localization.ToLocalizedTimeString(_settings.SessionThreshold) +
+                App.Loader.GetString("ContinuousReminderText2"),
+                _settings.IdleThreshold,
+                false);
+
+            _usageRecordManager.Record.SessionDuration = 0;
         }
     }
 }
